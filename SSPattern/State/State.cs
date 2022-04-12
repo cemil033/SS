93777a6150ac82fe3765ddcab7d8e22095929b55
cemil <cemiw033@gmail.com>
2022-04-13 00:44:32 +0400

    Strategy State

---
+++ SSPattern/State/State.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSPattern.State
+{
+    public interface State
+    {
+        void ExecuteCommand(Player player);
+    }
+    public class HealthyState : State
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is in Healthy State.");
+        }
+    }
+    public class HurtState : State
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is wounded. Please search health points");
+        }
+    }
+    public class DeadState : State
+    {
+        public void ExecuteCommand(Player player)
+        {
+            Console.WriteLine("The player is dead. Game Over.");
+        }
+    }
+    public class Player
+    {
+        State currentState;
+
+        public Player()
+        {
+            this.currentState = new HealthyState();
+        }
+
+        public void Bullethit(int bullets)
+        {
+            Console.WriteLine("Bullets hits to the player: " + bullets);
+            if (bullets < 5)
+                this.currentState = new HealthyState();
+            if (bullets >= 5 && bullets < 10)
+                this.currentState = new HurtState();
+            if (bullets >= 10)
+                this.currentState = new DeadState();
+
+            currentState.ExecuteCommand(this);
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Player player = new Player();
+                player.Bullethit(3);
+                player.Bullethit(9);
+                player.Bullethit(12);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SSPattern/Strategy/Strategy.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSPattern.Strategy
+{
+    public interface IEncodingStrategy
+    {
+        void EncodeValue(string value);
+    }
+    public class RSAEncodingStrategy : IEncodingStrategy
+    {
+        public void EncodeValue(string value)
+        {            
+            Console.WriteLine("Value {0} is Encoded using RSA Algorithm", value);
+        }
+    }
+    public class DESncodingStrategy : IEncodingStrategy
+    {
+        public void EncodeValue(string value)
+        {            
+            Console.WriteLine("Value {0} is Encoded using DES Algorithm", value);
+        }
+    }
+    public class BlowFishEncodingStrategy : IEncodingStrategy
+    {
+        public void EncodeValue(string value)
+        {
+            
+            Console.WriteLine("Value {0} is Encoded using BlowFish Algorithm", value);
+        }
+    }
+    public class Encoding
+    {
+        private IEncodingStrategy _encodeStrategy;
+
+        public Encoding(IEncodingStrategy encodeStrategy)
+        {
+            _encodeStrategy = encodeStrategy;
+        }
+
+        public void Encode(string value)
+        {
+            _encodeStrategy.EncodeValue(value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEncodingStrategy encodingStrategy = new RSAEncodingStrategy();
+            Encoding encoding = new Encoding(encodingStrategy);
+            encoding.Encode("10000011110");
+
+            encodingStrategy = new DESncodingStrategy();
+            encoding = new Encoding(encodingStrategy);
+            encoding.Encode("10000011110");
+
+            encodingStrategy = new BlowFishEncodingStrategy();
+            encoding = new Encoding(encodingStrategy);
+            encoding.Encode("10000011110");
+
+            Console.ReadLine();
+        }
+    }
+}
